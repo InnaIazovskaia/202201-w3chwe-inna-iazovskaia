e0fc8f4a35dee660e1c75057e5a0171bf0f8ddb3
Inna Iazovkaia <inna.0961@mail.ru>
2022-01-30 13:06:27 +0100

    added responsibility to creat PocemonCard to PageComponent

--- public/js/components/PageComponent/PageComponent.js
+++ public/js/components/PageComponent/PageComponent.js
@@ -1,18 +1,20 @@
 import Component from "../Component.js";
+import PokemonCardComponent from "../PokemonComponent/PokemonCardComponent.js";
 
 class PageComponent extends Component {
-  constructor(parentElement, pageTitle) {
+  constructor(parentElement, pageTitle, pokemonsData) {
     super(parentElement, "container", "div");
     this.pageTitle = pageTitle;
+    this.pokemonsData = pokemonsData;
 
     this.generateHTML();
   }
 
   generateHTML() {
     this.element.innerHTML = `
-      <header class="header row">
-        <img class="logo cal-2" src="./images/pokemon-logo.svg" alt="Pokémon logo" />
-          <nav class="nav cal-10">
+      <header class="header">
+        <img class="logo" src="./images/pokemon-logo.svg" alt="Pokémon logo" />
+          <nav class="nav">
             <a class="nav__item" href="#">All pokémons</a>
             <a class="nav__item" href="#">My pokémons</a>
             <a class="nav__item" href="#">Pokémon details</a>
@@ -22,6 +24,11 @@ class PageComponent extends Component {
         <h1 class="main__title">${this.pageTitle}</h1>
       </main>
     `;
+
+    const main = document.querySelector(".main");
+    this.pokemonsData.forEach((pokemon) => {
+      new PokemonCardComponent(main, pokemon.name, pokemon.img, "", "");
+    });
   }
 }
 
