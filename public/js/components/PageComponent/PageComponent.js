cdc2cfe511dd2fb7bf8e49c0c540808808001d19
Inna Iazovkaia <inna.0961@mail.ru>
2022-01-30 19:46:58 +0100

    corrected path of PokemonCardComponent in PageComponent.js

--- public/js/components/PageComponent/PageComponent.js
+++ public/js/components/PageComponent/PageComponent.js
@@ -1,5 +1,5 @@
 import Component from "../Component.js";
-import PokemonCardComponent from "../PokemonComponent/PokemonCardComponent.js";
+import PokemonCardComponent from "../PokemonCardComponent/PokemonCardComponent.js";
 
 class PageComponent extends Component {
   constructor(parentElement, pageTitle, pokemonsData) {
