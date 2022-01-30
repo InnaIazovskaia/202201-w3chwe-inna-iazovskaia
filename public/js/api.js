cb86277bde51b0c6815efdd70b5108a7bb8d6c89
Inna Iazovkaia <inna.0961@mail.ru>
2022-01-30 12:58:23 +0100

    created api.js

---
+++ public/js/api.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-use-before-define */
+const pokemonsObject = await getPokemonsList();
+
+async function getPokemonsList() {
+  const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon/");
+
+  const pokemonsList = await pokemons.json();
+
+  const pokemonsResults = pokemonsList.results.map((result) =>
+    getPokemonData(result.url)
+  );
+
+  const pokemonResponses = await Promise.all(pokemonsResults);
+
+  const pokemonData = pokemonResponses.map((pokemonResponse) =>
+    getPokemonInformation(pokemonResponse)
+  );
+
+  return pokemonData;
+}
+
+async function getPokemonData(url) {
+  const pokemonPesponse = await fetch(url);
+
+  return pokemonPesponse.json();
+}
+
+function getPokemonInformation(pokemonResponse) {
+  const pokemons = {
+    id: pokemonResponse.id,
+    name: pokemonResponse.name,
+    img: pokemonResponse.sprites.other.home.front_default,
+  };
+
+  return pokemons;
+}
+
+export default pokemonsObject;
